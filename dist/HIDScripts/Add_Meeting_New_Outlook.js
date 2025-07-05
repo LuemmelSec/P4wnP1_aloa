11643e2f166676cd9dc5abb7ea6eba33ab6f6d59
LuemmelSec <58529760+LuemmelSec@users.noreply.github.com>
2025-07-05 12:49:48 +0200

    Create Add_Meeting_New_Outlook.js

---
+++ dist/HIDScripts/Add_Meeting_New_Outlook.js
@@ -0,0 +1,40 @@
+layout('de');			
+typingSpeed(40,0)
+
+press("GUI r");
+delay(500);
+type("ms-outlook:");
+delay(500);
+press("ENTER");
+delay(4000);			//change to 5000 later on to wait if a completely new instance of Outlook needs to be spawned
+press("CTRL 2");
+delay(500);
+press("CTRL SHIFT q");
+delay(500);
+type("YOUR DESCRIPTION HERE");
+delay(500);
+press ("TAB");
+delay(500);
+press ("TAB");
+delay(500);
+type("a.b@company.com;b.c@company.de");
+delay(300);
+press ("TAB");
+delay(500);
+press ("TAB");
+delay(300);
+press ("TAB");
+delay(300);
+type("2025-07-12")		//the format needs to fit. Like German, US Layout etc
+delay(300);
+press ("TAB");
+delay(300);
+type("13:00");
+delay(300);
+press ("TAB");
+delay(300);
+type("14:00");
+delay(300);
+press("CTRL ENTER");
+delay(300);
+press("ALT F4");
