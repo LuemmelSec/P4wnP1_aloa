0ed4a181c94624dd0fef398e607ea0fde67b97b5
LuemmelSec <58529760+LuemmelSec@users.noreply.github.com>
2025-07-05 12:51:06 +0200

    Create Add_Meeting_Classic_Outlook.js

---
+++ dist/HIDScripts/Add_Meeting_Classic_Outlook.js
@@ -0,0 +1,42 @@
+layout('de');			
+typingSpeed(20,0)
+
+press("GUI r");
+delay(500);
+type("outlook\n")
+delay(5000);			//change to 5000 later on to wait if a completely new instance of Outlook needs to be spawned
+press("CTRL SHIFT q");
+delay(500);
+type("DESCRIPTION HERE");
+delay(500);
+press ("TAB");
+delay(500);
+type("a.b.@vcompany.com;c.d@company.de");
+delay(300);
+press ("TAB");
+delay(300);
+press ("TAB");
+delay(300);
+press ("TAB");
+delay(300);
+type("12/07/2025")     //the format needs to fit. Like German, US Layout etc 12.07.2025
+delay(300);
+press ("TAB");
+delay(300);
+press ("TAB");
+delay(300);
+type("13:00")         //the format needs to fit. Like German, US Layout etc
+delay(300);
+press ("TAB");
+delay(300);
+type("12/07/2025")     //the format needs to fit. Like German, US Layout etc
+delay(300);
+press ("TAB");
+delay(300);
+press ("TAB");
+delay(300);
+type("14:00")         //the format needs to fit. Like German, US Layout etc
+delay(300);
+press("ALT s");
+delay(300);
+press("ALT F4")
